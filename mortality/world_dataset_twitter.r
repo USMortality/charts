ba565e8fa3cc1604234d0c02e05cdab2391f7fdd
USMortality.com <USMortality@protonmail.com>
2022-12-06 12:33:07 -0700

    add twitter script

---
+++ mortality/world_dataset_twitter.r
@@ -0,0 +1,34 @@
+source("lib/common.r")
+
+my_app <- rtweet_bot(
+  api_key = Sys.getenv("TWITTER_API_KEY"),
+  api_secret = Sys.getenv("TWITTER_API_SECRET"),
+  access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_SECRET")
+)
+auth_as(my_app)
+
+world_max_date_old <- read.csv("out/mortality/world_max_date.csv")
+data_ytd <- as_tibble(read.csv("./out/mortality/world_ytd.csv"))
+
+df <- data_ytd %>%
+  group_by(iso3c, name) %>%
+  summarise(max = max(max_date))
+
+save_csv(df, "mortality/world_max_date")
+
+for (n in seq_len(nrow(df))) {
+  val <- df[n, ]
+  val_old <- world_max_date_old %>% filter(iso3c == val$iso3c)
+  if (val$max != val_old$max) {
+    print(paste(val$name, "changed"))
+    post_tweet(
+      paste0(
+        "Mortality Data for ", val$name,
+        " has been updated. Latest data now available through ", val$max, "."
+      ),
+      media = paste0("./out/mortality/", val$name, ".png"),
+      media_alt_text = paste("Mortality", val$name)
+    )
+  }
+}
