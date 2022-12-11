dfffca63c435de07d671e7f924d859e6394ce34f
USMortality.com <USMortality@protonmail.com>
2022-12-11 12:03:51 -0700

    simply twitter charts

--- mortality/world_dataset_twitter.r
+++ mortality/world_dataset_twitter.r
@@ -17,8 +17,6 @@ df <- data_ytd %>%
   group_by(iso3c, name) %>%
   summarise(max = max(max_date))
 
-save_csv(df, "mortality/world_max_date")
-
 tweet <- function(name, max) {
   post_tweet(
     paste0(
@@ -28,73 +26,32 @@ tweet <- function(name, max) {
     media = paste0("./out/mortality/", name, "/weekly_52w_sma_line.png"),
     media_alt_text = paste("Weekly Mortality (52W SMA)", name)
   )
+  Sys.sleep(10)
   post_tweet(
     paste("Weekly Mortality in", name),
     media = paste0("./out/mortality/", name, "/weekly_line.png"),
     media_alt_text = paste("Weekly Mortality", name),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
-  post_tweet(
-    paste("Monthly Mortality in", name),
-    media = paste0("./out/mortality/", name, "/monthly_line.png"),
-    media_alt_text = paste("Monthly Mortality", name),
-    in_reply_to_status_id = get_my_timeline()$id_str[1]
-  )
-  post_tweet(
-    paste("Quarterly Mortality in", name),
-    media = paste0("./out/mortality/", name, "/quarterly_line.png"),
-    media_alt_text = paste("Quarterly Mortality", name),
-    in_reply_to_status_id = get_my_timeline()$id_str[1]
-  )
-  post_tweet(
-    paste("Yearly Mortality in", name),
-    media = paste0("./out/mortality/", name, "/yearly_line.png"),
-    media_alt_text = paste("Yearly Mortality", name),
-    in_reply_to_status_id = get_my_timeline()$id_str[1]
-  )
-  post_tweet(
-    paste("YTD Mortality in", name, "through", max),
-    media = paste0("./out/mortality/", name, "/ytd_line.png"),
-    media_alt_text = paste("Mortality YTD", name),
-    in_reply_to_status_id = get_my_timeline()$id_str[1]
-  )
-  post_tweet(
-    paste("Mortality by Flu Season in", name),
-    media = paste0("./out/mortality/", name, "/fluseason_line.png"),
-    media_alt_text = paste("Mortality by Flu Season", name),
-    in_reply_to_status_id = get_my_timeline()$id_str[1]
-  )
-  post_tweet(
-    paste("Weekly Mortality - STL Decomposition in", name),
-    media = paste0("./out/mortality/", name, "/stl_line.png"),
-    media_alt_text = paste("Mortality by Flu Season", name),
-    in_reply_to_status_id = get_my_timeline()$id_str[1]
-  )
+  Sys.sleep(10)
   post_tweet(
     paste("Yearly Mortality in", name),
     media = paste0("./out/mortality/", name, "/yearly_bar.png"),
     media_alt_text = paste("Yearly Mortality", name),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
+  Sys.sleep(10)
   post_tweet(
     paste("YTD Mortality in", name, "through", max),
     media = paste0("./out/mortality/", name, "/ytd_bar.png"),
     media_alt_text = paste("YTD Mortality", name),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
+  Sys.sleep(10)
   post_tweet(
-    paste("Mortality in", name),
-    media = paste0("./out/mortality/", name, ".png"),
+    paste0("Find all charts at: https://www.mortality.watch/?country=", name),
+    media = paste0("./out/mortality/", name, "/collage.png"),
     media_alt_text = paste("Mortality", name),
-    in_reply_to_status_id = get_my_timeline()$id_str[1]
-  )
-  post_tweet(
-    paste0(
-      "Charts: https://www.mortality.watch/charts?prefix=mortality/", name,
-      "/ \n",
-      "Data: https://www.mortality.watch/data?prefix=mortality/ \n",
-      "Source Code: https://github.com/USMortality/charts/tree/master/mortality"
-    ),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
 }
@@ -110,3 +67,5 @@ for (n in seq_len(nrow(df))) {
     print(paste(val$name, "unchanged"))
   }
 }
+
+save_csv(df, "mortality/world_max_date")
