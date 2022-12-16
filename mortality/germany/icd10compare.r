2ba76be4bc066b43e8453838a3199951485ee6ce
USMortality.com <USMortality@protonmail.com>
2022-12-16 14:58:24 -0700

    add more queries

--- mortality/germany/icd10compare.r
+++ mortality/germany/icd10compare.r
@@ -3,15 +3,21 @@ source("lib/common.r")
 data <- read_remote("mortality/Germany/deaths_icd10_year.csv")
 
 df <- data %>%
-  filter(icd10 %in% c("T881", "U129")) %>%
-  group_by(year) %>%
+  mutate(icd10 = left(icd10, 3)) %>%
+  group_by(year, icd10) %>%
   summarise(deaths = sum(deaths)) %>%
-  mutate(date = ymd(year, truncated = 2L))
+  ungroup() %>%
+  pivot_wider(names_from = year, values_from = deaths)
 
-ggplot(df, aes(x = date, y = deaths)) +
+df <- df %>%
+  filter(df$`2019` >= 10000) %>%
+  pivot_longer(!icd10, names_to = "year", values_to = "deaths") %>%
+  relocate(year)
+
+ggplot(df, aes(x = year, y = deaths, group = icd10, group_name = "ICD-10", color = icd10)) +
   labs(
-    title = "Vaccine Deaths [Germany]",
-    subtitle = "ICD-10: U12.9 & T88.1; Source: Destatis",
+    title = "",
+    subtitle = "",
     y = "Deaths",
     x = "Year"
   ) +
@@ -30,8 +36,8 @@ ggplot(df, aes(x = date, y = deaths)) +
   #   se = FALSE,
   #   linetype = "solid"
   # ) +
-  geom_col(linewidth = 1.2, alpha = 0.7, fill = "#ff0000") +
-  twitter_theme() +
-  watermark(df$date, df$cmr) +
-  scale_x_date(date_breaks = "1 year", date_labels = "%Y", expand = c(0, 0)) +
+  geom_line(linewidth = 1.2, alpha = 0.7) +
+  # twitter_theme() +
+  # watermark(df$date, df$cmr) +
+  # scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
   theme(axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.5))
