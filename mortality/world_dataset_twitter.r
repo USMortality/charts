b5f40f59136a11bce19c1648dab11406fc2084f6
USMortality.com <USMortality@protonmail.com>
2022-12-23 12:32:38 -0700

    fix twitter

--- mortality/world_dataset_twitter.r
+++ mortality/world_dataset_twitter.r
@@ -1,5 +1,6 @@
 source("lib/common.r")
 
+url_base <- "https://s3.mortality.watch/charts/mortality/cmr/"
 my_app <- rtweet_bot(
   api_key = Sys.getenv("TWITTER_API_KEY"),
   api_secret = Sys.getenv("TWITTER_API_SECRET"),
@@ -18,39 +19,49 @@ df <- data_ytd %>%
   summarise(max = max(max_date_cmr))
 
 tweet <- function(name, max) {
+  url <- paste0(url_base, name, "/weekly_52w_sma_line.png")
+  download.file(url, "/tmp/tweet.png", mode = "wb")
   post_tweet(
     paste0(
       "Mortality Data for ", name,
       " has been updated. Latest data now available through ", max, "."
     ),
-    media = paste0("./out/mortality/", name, "/weekly_52w_sma_line.png"),
+    media = paste0("/tmp/tweet.png"),
     media_alt_text = paste("Weekly Mortality (52W SMA)", name)
   )
   Sys.sleep(10)
+  url <- paste0(url_base, name, "/weekly_line.png")
+  download.file(url, "/tmp/tweet.png", mode = "wb")
   post_tweet(
     paste("Weekly Mortality in", name),
-    media = paste0("./out/mortality/", name, "/weekly_line.png"),
+    media = paste0("/tmp/tweet.png"),
     media_alt_text = paste("Weekly Mortality", name),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
   Sys.sleep(10)
+  url <- paste0(url_base, name, "/yearly_bar.png")
+  download.file(url, "/tmp/tweet.png", mode = "wb")
   post_tweet(
     paste("Yearly Mortality in", name),
-    media = paste0("./out/mortality/", name, "/yearly_bar.png"),
+    media = paste0("/tmp/tweet.png"),
     media_alt_text = paste("Yearly Mortality", name),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
   Sys.sleep(10)
+  url <- paste0(url_base, name, "/ytd_bar.png")
+  download.file(url, "/tmp/tweet.png", mode = "wb")
   post_tweet(
     paste("YTD Mortality in", name, "through", max),
-    media = paste0("./out/mortality/", name, "/ytd_bar.png"),
+    media = paste0("/tmp/tweet.png"),
     media_alt_text = paste("YTD Mortality", name),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
   Sys.sleep(10)
+  url <- paste0(url_base, name, "/collage.png")
+  download.file(url, "/tmp/tweet.png", mode = "wb")
   post_tweet(
     paste0("Find all charts at: https://www.mortality.watch/?country=", name),
-    media = paste0("./out/mortality/", name, "/collage.png"),
+    media = paste0("/tmp/tweet.png"),
     media_alt_text = paste("Mortality", name),
     in_reply_to_status_id = get_my_timeline()$id_str[1]
   )
