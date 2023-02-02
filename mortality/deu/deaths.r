9b6cb7a2d869c67031f5ef9a9abd8eb96711561e
USMortality.com <USMortality@protonmail.com>
2023-02-01 17:52:08 -0800

    incl. DE states mortality from 2000

--- mortality/deu/deaths.r
+++ mortality/deu/deaths.r
@@ -1,36 +1,46 @@
 source("lib/common.r")
 
 # National
-df <- read_excel(
+df1 <- read_excel(
+  "./data_static/sonderauswertung-sterbefaelle-endgueltige-daten.xlsx",
+  sheet = "D_2000_2015_KW_AG_Ins",
+  range = "B9:BE9999"
+)
+df2 <- read_excel(
   "./data/sonderauswertung-sterbefaelle.xlsx",
   sheet = "D_2016_2023_KW_AG_Ins",
   range = "B9:BE9999"
 )
 
-df1 <- df %>%
-  mutate(across(.cols = 3:ncol(df), .fns = as.numeric)) %>%
-  pivot_longer(cols = 3:ncol(df), names_to = "week", values_to = "deaths") %>%
+result1 <- rbind(df1, df2) %>%
+  mutate(across(.cols = 3:ncol(df1), .fns = as.numeric)) %>%
+  pivot_longer(cols = 3:ncol(df1), names_to = "week", values_to = "deaths") %>%
   setNames(c("year", "age_group", "week", "deaths")) %>%
   mutate(jurisdiction = "Deutschland", .after = "year")
 
 # States
-df <- read_excel(
+df1 <- read_excel(
+  "./data_static/sonderauswertung-sterbefaelle-endgueltige-daten.xlsx",
+  sheet = "BL_2000_2015_KW_AG_Ins",
+  range = "B9:BE9999"
+)
+df2 <- read_excel(
   "./data/sonderauswertung-sterbefaelle.xlsx",
   sheet = "BL_2016_2023_KW_AG_Ins",
   range = "B9:BE9999"
 )
-
-df2 <- df %>%
-  mutate(across(.cols = 4:ncol(df), .fns = as.numeric)) %>%
-  pivot_longer(cols = 4:ncol(df), names_to = "week", values_to = "deaths") %>%
+result2 <- rbind(df1, df2) %>%
+  mutate(across(.cols = 4:ncol(df1), .fns = as.numeric)) %>%
+  pivot_longer(cols = 4:ncol(df1), names_to = "week", values_to = "deaths") %>%
   setNames(c("year", "jurisdiction", "age_group", "week", "deaths"))
 
-df <- rbind(df1, df2) %>%
+df <- rbind(result1, result2) %>%
   mutate(
     year = as.integer(year),
     week = as.integer(week),
     deaths = as.integer(deaths)
   ) %>%
-  filter(!is.na(deaths))
+  filter(!is.na(deaths)) %>%
+  arrange(year, jurisdiction, age_group, week)
 
 save_csv(df, "mortality/deu/deaths")
--- mortality/deu/mortality_states.r
+++ mortality/deu/mortality_states.r
@@ -1,40 +1,11 @@
 source("lib/common.r")
 
+df <- read_remote("mortality/deu/deaths.csv")
 de_states <- as_tibble(read.csv("./data_static/deu_states_iso3c.csv"))
-
-df1 <- read_excel(
-  "./data_static/sonderauswertung-sterbefaelle-endgueltige-daten.xlsx",
-  sheet = "BL_2000_2015_KW_AG_Ins",
-  range = "B9:BE9999"
-)
-
-df2 <- read_excel(
-  "./data/sonderauswertung-sterbefaelle.xlsx",
-  sheet = "BL_2016_2023_KW_AG_Ins",
-  range = "B9:BE9999"
-)
-
-df1 <- df1 %>%
-  mutate(across(.cols = 4:ncol(df1), .fns = as.numeric)) %>%
-  pivot_longer(cols = 4:ncol(df1), names_to = "week", values_to = "deaths") %>%
-  setNames(c("year", "jurisdiction", "age_group", "week", "deaths"))
-
-df2 <- df2 %>%
-  mutate(across(.cols = 4:ncol(df2), .fns = as.numeric)) %>%
-  pivot_longer(cols = 4:ncol(df2), names_to = "week", values_to = "deaths") %>%
-  setNames(c("year", "jurisdiction", "age_group", "week", "deaths"))
-
-df <- rbind(df1, df2) %>%
-  mutate(
-    year = as.integer(year),
-    week = as.integer(week),
-    deaths = as.integer(deaths)
-  ) %>%
-  inner_join(de_states, by = "jurisdiction") %>%
-  filter(!is.na(deaths))
 
 # Format: iso3c, date, deaths
 dd_de <- df %>%
+  inner_join(de_states, by = "jurisdiction") %>%
   filter(age_group == "Insgesamt") %>%
   mutate(
     date = make_yearweek(year = year, week = week),
