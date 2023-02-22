f9aa04e303773c57adc039f55727fb334a954a13
USMortality.com <USMortality@protonmail.com>
2023-02-22 00:12:23 -0800

    add CI

--- covid19/gbr/ve.r
+++ covid19/gbr/ve.r
@@ -34,20 +34,36 @@ vaxxed <- data |>
   filter(!is.na(age_group)) |>
   mutate(date = yearmonth(date)) |>
   group_by(date, age_group) |>
-  summarise(vaxxed = mean(vaxxed))
+  summarise(
+    vaxxed_lower = min(vaxxed),
+    vaxxed_upper = max(vaxxed),
+    vaxxed = mean(vaxxed)
+  )
 
 # Deaths
 ve <- deaths |>
   inner_join(vaxxed, by = c("date", "age_group")) |>
-  mutate(population = ifelse(
-    vaxx_status == "Unvaccinated",
-    1 - vaxxed,
-    vaxxed
-  )) |>
-  select(-vaxxed) |>
+  mutate(
+    population = ifelse(
+      vaxx_status == "Unvaccinated",
+      1 - vaxxed,
+      vaxxed
+    ),
+    population_lower = ifelse(
+      vaxx_status == "Unvaccinated",
+      1 - vaxxed_lower,
+      vaxxed_lower
+    ),
+    population_upper = ifelse(
+      vaxx_status == "Unvaccinated",
+      1 - vaxxed_upper,
+      vaxxed_upper
+    )
+  ) |>
+  select(-vaxxed, -vaxxed_lower, -vaxxed_upper) |>
   pivot_wider(
     names_from = vaxx_status,
-    values_from = c(deaths, population)
+    values_from = c(deaths, population, population_lower, population_upper)
   ) |>
   set_names(c(
     "date",
@@ -55,13 +71,21 @@ ve <- deaths |>
     "deaths_unvaxx",
     "deaths_vaxx",
     "population_unvaxx",
-    "population_vaxx"
+    "population_vaxx",
+    "population_unvaxx_lower",
+    "population_vaxx_lower",
+    "population_unvaxx_upper",
+    "population_vaxx_upper"
   )) |>
   mutate(
     ve = 1 - (deaths_vaxx / population_vaxx) /
-      (deaths_unvaxx / population_unvaxx)
+      (deaths_unvaxx / population_unvaxx),
+    ve_lower = 1 - (deaths_vaxx / population_vaxx_lower) /
+      (deaths_unvaxx / population_unvaxx_lower),
+    ve_upper = 1 - (deaths_vaxx / population_vaxx_upper) /
+      (deaths_unvaxx / population_unvaxx_upper)
   ) |>
-  select(1, 2, 7)
+  select(date, age_group, ve, ve_lower, ve_upper)
 
 ggplot(
   ve,
@@ -74,6 +98,11 @@ ggplot(
     y = "Vaccine Efficacy"
   ) +
   geom_line(linewidth = 1) +
+  geom_ribbon(aes(
+    ymin = ve_lower,
+    ymax = ve_upper,
+    fill = age_group
+  ), alpha = .3, linetype = 0) +
   twitter_theme() +
   geom_hline(yintercept = 0) +
   watermark(df$yearmonth, df$value_p) +
@@ -83,7 +112,6 @@ ggplot(
     limits = c(-1, 1)
   ) +
   theme(legend.position = "top")
-
 
 options(vsc.dev.args = list(width = 1920, height = 1080, res = 72 * sf))
 ggplot(
@@ -97,6 +125,11 @@ ggplot(
     y = "Vaccine Efficacy"
   ) +
   geom_line(linewidth = 1) +
+  geom_ribbon(aes(
+    ymin = ve_lower,
+    ymax = ve_upper,
+    fill = age_group
+  ), alpha = .3, linetype = 0) +
   twitter_theme() +
   geom_hline(yintercept = 0) +
   watermark(df$yearmonth, df$value_p) +
