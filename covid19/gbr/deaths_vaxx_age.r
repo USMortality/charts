d011fa16ddf1ffdc60489a38796208a1cf498979
USMortality.com <USMortality@protonmail.com>
2023-02-24 14:47:17 -0800

    add by gender, and totals/vaxx

--- covid19/gbr/deaths_vaxx_age.r
+++ covid19/gbr/deaths_vaxx_age.r
@@ -3,7 +3,7 @@ source("lib/common.r")
 source("covid19/gbr/vaxx.r")
 source("covid19/gbr/deaths_by_vaxx.r")
 
-options(vsc.dev.args = list(width = 1920, height = 1080, res = 72 * sf))
+# options(vsc.dev.args = list(width = 1920, height = 1080, res = 72 * sf))
 
 deaths <- df |>
   filter(type == "All causes") |>
@@ -26,7 +26,7 @@ max_d <- max(data$deaths)
 
 ggplot(
   data,
-  aes(x = date, group = age_group, color = age_group, fill = age_group)
+  aes(x = date, group = age_group)
 ) +
   labs(
     title = "All-Cause Deaths & Cumulative COVID-19 Vaccination [England]",
@@ -55,3 +55,77 @@ ggplot(
     legend.position = "top",
     axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.5)
   )
+
+make_chart <- function(data, age_group) {
+  max_p <- 1
+  max_d <- max(df$deaths)
+  ggplot(
+    data,
+    aes(x = date, y = deaths)
+  ) +
+    labs(
+      title = "All-Cause Deaths & Cumulative COVID-19 Vaccination [England]",
+      subtitle = paste0("Age-Group | ", age_group, " | Source: ons.gov.uk"),
+      x = "Month of Year",
+      y = "Deaths"
+    ) +
+    # geom_line(aes(y = deaths), linewidth = 2, color = "darkblue") +
+    geom_col(aes(y = deaths), fill = "darkblue") +
+    geom_smooth(method = "lm") +
+    geom_line(aes(y = vaxxed_pct / (max_p / max_d)), linewidth = 2, color = "#ff0000") +
+    scale_y_continuous(
+      # labels = label_number(suffix = "k", scale = 1e-3),
+      sec.axis = sec_axis(
+        ~ . * (max_p / max_d),
+        name = "Vaccinated (1st)",
+        labels = scales::label_percent()
+      )
+    ) +
+    twitter_theme() +
+    scale_x_yearmonth(date_breaks = "2 months", date_labels = "%Y/%m") +
+    theme(
+      # panel.spacing = unit(0.3, "in"),
+      legend.position = "top",
+      axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.5)
+    )
+}
+
+make_chart2 <- function(data, age_group) {
+  ggplot(data, aes(x = month, y = change, fill = change)) +
+    labs(
+      title = "Change in Monthly All Cause Deaths 2022/2021 [England]",
+      subtitle = paste0("Age-Group | ", age_group, " | Source: ons.gov.uk"),
+      x = "Month of Year",
+      y = "Change from 2021 to 2022"
+    ) +
+    geom_bar(stat = "identity") +
+    scale_fill_gradient2(low = "darkgreen", mid = "snow3", high = "darkred", space = "Lab") +
+    twitter_theme() +
+    scale_x_yearmonth(date_breaks = "2 months", date_labels = "%Y/%m") +
+    scale_y_continuous(
+      labels = scales::percent_format(scale = 100),
+      limits = c(-1, 1)
+    ) +
+    theme(
+      # panel.spacing = unit(0.3, "in"),
+      legend.position = "top",
+      axis.text.x = element_text(angle = 30, hjust = 0.5, vjust = 0.5)
+    )
+}
+
+for (ag in unique(data$age_group)) {
+  df <- data |> filter(age_group == ag)
+  chart <- make_chart(df, ag)
+  save_chart(chart, paste0("acd_vaxx_", ag), 2, FALSE)
+}
+
+# Month to Monty, Yearly increase
+for (ag in unique(data$age_group)) {
+  df <- data |>
+    filter(age_group == ag) |>
+    mutate(year = year(date), month = month(date)) |>
+    reshape2::dcast(month ~ year, value.var = "deaths") |>
+    mutate(change = `2022` / `2021` - 1)
+  chart <- make_chart2(df, ag)
+  save_chart(chart, paste0("acd_vaxx_chg", ag), 2, FALSE)
+}
---
+++ covid19/gbr/mr_by_age_gender_vaxx.r
@@ -0,0 +1,91 @@
+source("lib/common.r")
+options(vsc.dev.args = list(width = 1920, height = 1080, res = 72 * sf))
+
+make_chart <- function(data, title) {
+  ggplot(data, aes(
+    x = date, y = asmr_py, color = sex, group = interaction(vaxx_status, sex)
+  )) +
+    geom_line(aes(linetype = vaxx_status, color = sex)) +
+    # geom_point(aes(color = sex)) +
+    labs(
+      title = title,
+      subtitle = "Source: ons.gov.England",
+      x = "Month of Year",
+      y = "Mortality rate / 100,000 person-years"
+    ) +
+    twitter_theme() +
+    watermark(df$yearmonth, df$value_p) +
+    scale_y_continuous(
+      labels = label_number(suffix = "k", scale = 1e-3),
+      # limits = c(0, 2500)
+    ) +
+    facet_wrap(vars(age_group), scales = "free") +
+    theme(
+      # panel.spacing = unit(0.3, "in"),
+      legend.position = "top"
+    )
+}
+
+data <- read_excel(
+  "./data/uk_acm_vaxx.xlsx",
+  sheet = "Table 4",
+  range = "A4:L9999"
+)
+
+df <- data |>
+  select(1, 2, 3, 4, 5, 6, 9) |>
+  setNames(c(
+    "sex",
+    "type",
+    "year",
+    "month",
+    "age_group",
+    "vaxx_status",
+    "asmr_py"
+  )) |>
+  mutate(date = make_yearmonth(
+    year = year,
+    month = match(month, month.name)
+  ), .after = type) |>
+  mutate(asmr_py = as.double(asmr_py)) |>
+  select(-year, -month)
+
+# Unvaccinated vs First
+data <- df |>
+  filter(
+    type == "All causes",
+    # type == "Deaths involving COVID-19",
+    vaxx_status %in% c(
+      "Unvaccinated",
+      "First dose, at least 21 days ago"
+    )
+  ) |>
+  arrange(age_group, sex, date, vaxx_status)
+make_chart(data, "Monthly All-Cause AMSR/PY by Gender & Vaccination Status [England]")
+
+# Unvaccinated vs Second
+data <- df |>
+  filter(
+    type == "All causes",
+    # type == "Deaths involving COVID-19",
+    vaxx_status %in% c(
+      "Unvaccinated",
+      "Second dose, at least 21 days ago"
+    ),
+    # age_group == "80-89"
+  ) |>
+  arrange(age_group, sex, date, vaxx_status)
+make_chart(data, "Monthly All-Cause AMSR/PY by Gender & Vaccination Status [England]")
+
+# Unvaccinated vs Boosted
+data <- df |>
+  filter(
+    type == "All causes",
+    # type == "Deaths involving COVID-19",
+    vaxx_status %in% c(
+      "Unvaccinated",
+      "Third dose, at least 21 days ago"
+    )
+  ) |>
+  arrange(age_group, sex, date, vaxx_status)
+make_chart(data, "Monthly All-Cause AMSR/PY by Gender & Vaccination Status [England]")
