1718a585b5dac40a8ab1604e46d99f40afbb51af
USMortality.com <USMortality@protonmail.com>
2023-05-20 20:59:22 -0700

    add 5y age bins

--- mortality/usa/deaths_yearly.r
+++ mortality/usa/deaths_yearly.r
@@ -23,7 +23,43 @@ totals <- rbind(
   arrange(iso3c, date) |>
   distinct(iso3c, date, .keep_all = TRUE)
 
-# By age group
+processAgeGroups <- function(age_groups, ag) {
+  # 1999-2020
+  ag1 <- data.frame()
+  for (age_group in age_groups) {
+    data <- read.csv(paste0(
+      "../wonder_dl/out/yearly/us_states_1999_2020_",
+      age_group,
+      ".csv"
+    ))
+    ag1 <- rbind(ag1, parseData(data, "State", age_group))
+  }
+
+  # 2020+
+  ag2 <- data.frame()
+  for (age_group in age_groups) {
+    data <- read.csv(paste0(
+      "../wonder_dl/out/yearly/us_states_2020_n_",
+      age_group,
+      ".csv"
+    ))
+    ag2 <- rbind(ag2, parseData(data, "Residence.State", age_group))
+  }
+
+  totals_ag <- rbind(ag1, ag2) |>
+    as_tibble() |>
+    arrange(iso3c, date, age_group) |>
+    distinct(iso3c, date, age_group, .keep_all = TRUE)
+
+  result <- rbind(totals, totals_ag) |>
+    arrange(iso3c, date, age_group) |>
+    distinct(iso3c, date, age_group, .keep_all = TRUE) |>
+    complete(iso3c, date, age_group)
+
+  save_csv(result, paste0("deaths/usa/yearly_", ag), upload = TRUE)
+}
+
+# By 10y age group
 age_groups <- c(
   "0_9",
   "10_19",
@@ -37,37 +73,30 @@ age_groups <- c(
   "90_100",
   "NS"
 )
+processAgeGroups(age_groups, "10y")
 
-# 1999-2020
-ag1 <- data.frame()
-for (age_group in age_groups) {
-  data <- read.csv(paste0(
-    "../wonder_dl/out/yearly/us_states_1999_2020_",
-    age_group,
-    ".csv"
-  ))
-  ag1 <- rbind(ag1, parseData(data, "State", age_group))
-}
-
-# 2020+
-ag2 <- data.frame()
-for (age_group in age_groups) {
-  data <- read.csv(paste0(
-    "../wonder_dl/out/yearly/us_states_2020_n_",
-    age_group,
-    ".csv"
-  ))
-  ag2 <- rbind(ag2, parseData(data, "Residence.State", age_group))
-}
-
-totals_ag <- rbind(ag1, ag2) |>
-  as_tibble() |>
-  arrange(iso3c, date, age_group) |>
-  distinct(iso3c, date, age_group, .keep_all = TRUE)
-
-result <- rbind(totals, totals_ag) |>
-  arrange(iso3c, date, age_group) |>
-  distinct(iso3c, date, age_group, .keep_all = TRUE) |>
-  complete(iso3c, date, age_group)
-
-save_csv(result, "deaths/usa/yearly_10y", upload = TRUE)
+# By 5y age group
+age_groups <- c(
+  "0_4",
+  "5_9",
+  "10_14",
+  "15_19",
+  "20_24",
+  "25_29",
+  "30_34",
+  "35_39",
+  "40_44",
+  "45_49",
+  "50_54",
+  "55_59",
+  "60_64",
+  "65_69",
+  "70_74",
+  "75_79",
+  "80_84",
+  "85_89",
+  "90_94",
+  "95_100",
+  "NS"
+)
+processAgeGroups(age_groups, "5y")
