65d7d91d1fb74a8515a0ce5289842a5695741197
USMortality.com <USMortality@protonmail.com>
2023-12-08 22:26:11 +0800

    disable progress bar on single core

--- mortality/world_dataset.r
+++ mortality/world_dataset.r
@@ -1,11 +1,12 @@
 source("lib/common.r")
 source("lib/asmr.r")
 
-# Multithreading
 options(progressr.enable = TRUE)
+
+# Multithreading
 n_cores <- detectCores()
 if (n_cores > 2) {
-  plan(multisession, workers = max(1, cores - 1))
+  plan(multisession, workers = max(1, n_cores - 1))
 } else {
   plan(sequential)
 }
@@ -52,72 +53,81 @@ save_info(
   upload = FALSE
 )
 rm(iso3c_jurisdiction)
-countries <- unique(data$iso3c)
-with_progress({
-  p <- progressor(steps = length(countries))
+
+process_country <- function(df) {
+  iso3c <- df[1, ]$iso3c
+  print(paste0("ISO: ", iso3c))
+  # Make furr pull in these functions
+  fluseason <- fluseason
+  midyear <- midyear
+  dd <- df |>
+    mutate(cmr = deaths / population * 100000) |>
+    expand_daily()
+  dd_all <- dd |>
+    filter(age_group == "all") |>
+    arrange(date, desc(type)) |>
+    distinct(iso3c, date, .keep_all = TRUE)
+  dd_age <- dd |>
+    filter(age_group != "all") |>
+    arrange(date, desc(type), age_group)
+  dd_asmr <- dd_age
+  if (nrow(dd_age)) {
+    dd_asmr <- dd_age |>
+      group_by(iso3c, type, n_age_groups, source) |>
+      group_modify(~ calculate_asmr_variants(.x), .keep = TRUE) |>
+      ungroup() |>
+      filter(type == max(type), .by = c(iso3c, date)) |>
+      filter(n_age_groups == max(n_age_groups), .by = c(iso3c, date)) |>
+      distinct(iso3c, date, .keep_all = TRUE) |>
+      arrange(date, type)
+    dd_asmr$age_group <- "all"
+  }
+
+  for (ag in unique(dd$age_group)) {
+    print(paste0("Age Group: ", ag))
+    if (ag == "all") {
+      write_dataset(
+        iso3c, ag,
+        weekly = summarize_data_all(dd_all, dd_asmr, "yearweek"),
+        monthly = summarize_data_all(dd_all, dd_asmr, "yearmonth"),
+        quarterly = summarize_data_all(dd_all, dd_asmr, "yearquarter"),
+        yearly = summarize_data_all(dd_all, dd_asmr, "year"),
+        by_fluseason <- summarize_data_all(dd_all, dd_asmr, "fluseason"),
+        by_midyear = summarize_data_all(dd_all, dd_asmr, "midyear")
+      )
+    } else {
+      dd_ag_f <- dd_age |>
+        filter(age_group == ag) |>
+        distinct(iso3c, date, age_group, .keep_all = TRUE)
+      write_dataset(
+        iso3c, ag,
+        weekly = summarize_data_by_time(dd_ag_f, "yearweek"),
+        monthly = summarize_data_by_time(dd_ag_f, "yearmonth"),
+        quarterly = summarize_data_by_time(dd_ag_f, "yearquarter"),
+        yearly = summarize_data_by_time(dd_ag_f, "year"),
+        by_fluseason = summarize_data_by_time(dd_ag_f, "fluseason"),
+        by_midyear = summarize_data_by_time(dd_ag_f, "midyear")
+      )
+    }
+  }
+}
+
+if (Sys.getenv("CI") != 1) {
+  countries <- unique(data$iso3c)
+  with_progress({
+    p <- progressor(steps = length(countries))
+    data |>
+      group_split(iso3c) |>
+      future_walk(~ {
+        process_country(.x)
+        p()
+      })
+  })
+} else {
   data |>
     group_split(iso3c) |>
-    future_walk(
-      ~ {
-        iso3c <- .x[1, ]$iso3c
-        print(paste0("ISO: ", iso3c))
-        # Make furr pull in these functions
-        fluseason <- fluseason
-        midyear <- midyear
-        dd <- .x |>
-          mutate(cmr = deaths / population * 100000) |>
-          expand_daily()
-        dd_all <- dd |>
-          filter(age_group == "all") |>
-          arrange(date, desc(type)) |>
-          distinct(iso3c, date, .keep_all = TRUE)
-        dd_age <- dd |>
-          filter(age_group != "all") |>
-          arrange(date, desc(type), age_group)
-        dd_asmr <- dd_age
-        if (nrow(dd_age)) {
-          dd_asmr <- dd_age |>
-            group_by(iso3c, type, n_age_groups, source) |>
-            group_modify(~ calculate_asmr_variants(.x), .keep = TRUE) |>
-            ungroup() |>
-            filter(type == max(type), .by = c(iso3c, date)) |>
-            filter(n_age_groups == max(n_age_groups), .by = c(iso3c, date)) |>
-            distinct(iso3c, date, .keep_all = TRUE) |>
-            arrange(date, type)
-          dd_asmr$age_group <- "all"
-        }
-
-        for (ag in unique(dd$age_group)) {
-          print(paste0("Age Group: ", ag))
-          if (ag == "all") {
-            write_dataset(
-              iso3c, ag,
-              weekly = summarize_data_all(dd_all, dd_asmr, "yearweek"),
-              monthly = summarize_data_all(dd_all, dd_asmr, "yearmonth"),
-              quarterly = summarize_data_all(dd_all, dd_asmr, "yearquarter"),
-              yearly = summarize_data_all(dd_all, dd_asmr, "year"),
-              by_fluseason <- summarize_data_all(dd_all, dd_asmr, "fluseason"),
-              by_midyear = summarize_data_all(dd_all, dd_asmr, "midyear")
-            )
-          } else {
-            dd_ag_f <- dd_age |>
-              filter(age_group == ag) |>
-              distinct(iso3c, date, age_group, .keep_all = TRUE)
-            write_dataset(
-              iso3c, ag,
-              weekly = summarize_data_by_time(dd_ag_f, "yearweek"),
-              monthly = summarize_data_by_time(dd_ag_f, "yearmonth"),
-              quarterly = summarize_data_by_time(dd_ag_f, "yearquarter"),
-              yearly = summarize_data_by_time(dd_ag_f, "year"),
-              by_fluseason = summarize_data_by_time(dd_ag_f, "fluseason"),
-              by_midyear = summarize_data_by_time(dd_ag_f, "midyear")
-            )
-          }
-        }
-        p()
-      }
-    )
-})
+    walk(process_country)
+}
 
 print("Finished.")
 
