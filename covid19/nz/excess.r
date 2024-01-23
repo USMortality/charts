56ca1415ffb6f30bff0327e655ca4f48ff57d711
USMortality.com <USMortality@protonmail.com>
2024-01-23 00:46:58 -0800

    fixes

--- covid19/nz/excess.r
+++ covid19/nz/excess.r
@@ -1,13 +1,13 @@
 source("./lib/common.r")
 
 # Yearly NZ data
-df <- read.csv("https://s3.mortality.watch/data/mortality/world_fluseason.csv")
+df <- read.csv("https://s3.mortality.watch/data/mortality/NZL/fluseason.csv")
 nz <- df |>
     filter(iso3c == "NZL") |>
     as_tibble() |>
     select(date, deaths) |>
     mutate(year = as.integer(left(date, 4))) |>
-    filter(year >= 2009)
+    filter(year >= 2010)
 
 # Split data into training and test
 df_train <- nz |> filter(year <= 2019)
@@ -22,15 +22,15 @@ fc_sum_mean <- sum(oo$fit)
 fc_sum_variance <- sum(oo$var.fit)
 
 n <- ncol(lengths(oo$var.fit))
-res <- agg_pred(rep.int(x = 1, 3), oo, alpha = .99)
+res <- agg_pred(rep.int(x = 1, length(oo$fit)), oo, alpha = .95)
 
 print(paste0(
-    "Actual: ", sum(df_test$deaths),
+    "Actual deaths: ", sum(df_test$deaths),
     ", Expected: ", round(fc_sum_mean),
-    ", 99%PI[", round(res$PI[1]), ",", round(res$PI[2]), "]"
+    ", 95%PI[", round(res$PI[1]), ",", round(res$PI[2]), "]"
 ))
 print(paste0(
-    "Excess: ", round(sum(df_test$deaths) - fc_sum_mean),
-    ", 99%CI[", sum(df_test$deaths) - round(res$PI[2]), ",",
+    "Excess deaths: ", round(sum(df_test$deaths) - fc_sum_mean),
+    ", 95%CI[", sum(df_test$deaths) - round(res$PI[2]), ",",
     sum(df_test$deaths) - round(res$PI[1]), "]"
 ))
